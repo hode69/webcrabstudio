bc2ed772d206f8bcc620bd5d23c921a963b8c0fa
hode69 <43639379+hode69@users.noreply.github.com>
2024-11-26 14:41:09 +0800

    12.2

---
+++ assets/script/navbar.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+    const toggleNavbar = document.querySelector(".toggle-navbar");
+    const toggleNavbarIcon = document.querySelector(".toggle-navbar i");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+
+    if (!toggleNavbar) {
+        console.error("Toggle navbar not found!");
+        return;
+    }
+    else {
+        console.log("Toggle Navbar:", toggleNavbar);
+
+    }
+
+    const toggleMenu = function () {
+        if (!toggleNavbar) {
+            alert("Toggle navbar element is missing!");
+        } else {
+            dropdownMenu.classList.toggle("open");
+            const isOpen = dropdownMenu.classList.contains("open");
+            toggleNavbarIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+        }
+    };
+
+    // Use matchMedia to ensure it only works on smaller screens
+    function windowMode() {
+        const mediaQuery = window.matchMedia("(max-width: 768px)");
+        if (mediaQuery.matches) {
+            console.log("Media query matched. Mobile mode.");
+            toggleNavbar.addEventListener("pointerdown", toggleMenu);
+        } else {
+            console.log("Media query did not match. Desktop mode.");
+        }
+    }
+
+    window.addEventListener("resize", windowMode);
+    windowMode(); // Initial mode check
+};
